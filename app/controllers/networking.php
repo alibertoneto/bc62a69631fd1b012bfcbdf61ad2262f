6da24a7b1f7f41ced7e2f9749ec0ac061f86b7b3
Jeferson Rossini <jrossini94@gmail.com>
2018-02-03 09:26:12 -0200

    criação da pagina network

---
+++ app/controllers/networking.php
@@ -0,0 +1,28 @@
+<?
+	namespace Controllers;
+	use Core;
+
+	class Networking extends Core\Controller{
+		
+		function index(){
+
+			// add tag header
+			$assets = [
+				'js' => [
+					//'http://malsup.github.com/jquery.cycle2.js',
+                    "https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.6.5/angular.min.js",
+                    "public/js/service/atividades.js",
+                    "public/js/app/atividades.js",
+				],
+                "css" => [
+                    "public/css/pages.css"
+                ]
+			];
+
+			$data['config']['assets'] = $this->view->setAssets($assets);
+
+			// RENDER PAGE HTML 
+			$this->view->render('Networking/index', $data);
+		}
+
+	}
