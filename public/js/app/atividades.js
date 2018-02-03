916c362e7a385df5ff5b76cf1850ee0b0c589374
Aliberto <aliberto.neto@gmail.com>
2018-02-03 01:53:34 -0200

    Conexão com API em nova tela

---
+++ public/js/app/atividades.js
@@ -0,0 +1,27 @@
+var app = angular.module('app', ['atividades']);
+app.controller('AppCtrl', function($scope, AppService) {
+    $scope.nome = 'Erick';
+    $scope.items = [];
+    $scope.ativo = false;
+    $scope.atividade = {};
+
+    AppService.listar().then(function(resultado) {
+        //$scope.items = resultado.results;
+        var i = 0;
+        $.each(resultado.results, function (indice, valor) {
+            if (i < 6) {
+                $scope.items[i] = valor;
+            }
+            i++;
+        });
+        console.log('$scope.items', $scope.items);
+    });
+    $scope.clickDetalhes = clickDetalhes;
+    function clickDetalhes(nome) {
+        $scope.ativo = true;
+        AppService.detalhes(nome).then(function(resultado) {
+            $scope.atividade = resultado;
+            console.log('$scope.atividade', $scope.atividade);
+        });
+    }
+});
